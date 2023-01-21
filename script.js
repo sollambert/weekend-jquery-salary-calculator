994b2144c9e061e417cf01020b47e9c2fe4b8037
Solomon Lambert <sol.lambert@gmail.com>
2023-01-20 18:17:40 -0600

    create getters for input elements

--- script.js
+++ script.js
@@ -3,6 +3,33 @@ $(function() {
     onReady();
 });
 
-function onReady() {
+let employees = []
 
+function onReady() {
+    $('#employee-form').on('submit', onSubmit);
+}
+
+function onSubmit(evt) {
+    evt.preventDefault();
+    console.log('testing');
+
+    let first = $('#input-first-name').val();
+    let last = $('#input-last-name').val();
+    let id = $('#input-id').val();
+    let title = $('#input-title').val();
+    let salary = $('#input-salary').val();
+    let employee = {
+        first: first,
+        last: last,
+        id: id,
+        title: title,
+        salary: salary
+    }
+
+    employees.push(employee);
+    render();
+}
+
+function render() {
+    
 }
